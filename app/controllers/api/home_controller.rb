a671859ae549022bad6c5eb1d7edd7065f2e40ad
Stinky Pete <mbpeterson19@gmail.com>
2018-11-05 23:25:18 -0700

    asdf

--- app/controllers/api/home_controller.rb
+++ app/controllers/api/home_controller.rb
@@ -1,12 +1,12 @@
 class Api::HomeController < ApplicationController
   def index
-    bulletins = Bulletin.all
+    bulletin = Bulletin.all
     newsletter = Newsletter.last
     instagram = HTTParty.get("https://api.instagram.com/v1/users/self/media/recent/?access_token=",
     {query:
       {access_token: ENV['INSTAGRAM_ACCESS_TOKEN'],
             count:  '1'}
           })
-      render json: {photos: instagram, bulletins: bulletins, newsletter: newsletter} 
+      render json: {photos: instagram, bulletins: bulletin, newsletter: newsletter} 
   end
 end
